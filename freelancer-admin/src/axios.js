2b8c0c69d991328cf1930455b4911aca7a067baa
mohsin khan <117224062+MohsinK443@users.noreply.github.com>
2023-05-23 15:41:16 +0500

    Register view done

--- freelancer-admin/src/axios.js
+++ freelancer-admin/src/axios.js
@@ -1,6 +1,6 @@
-// config.js
+//axios.js
 
 export default {
-  baseURL: 'http://localhost:8081/api', // Change this to your server URL.
+  baseURL: 'http://localhost:8000/api', // Change this to your server URL.
   timeout: 5000 // Set a timeout value if needed
 };
--- freelancer-admin/src/pages/Register.vue
+++ freelancer-admin/src/pages/Register.vue
@@ -3,6 +3,10 @@
     <h2>Register</h2>
     <form @submit.prevent="register">
       <div class="form-group">
+        <label for="name">Name:</label>
+        <input type="text" class="form-control" id="name" v-model="name" required>
+      </div>
+      <div class="form-group">
         <label for="email">Email:</label>
         <input type="email" class="form-control" id="email" v-model="email" required>
       </div>
@@ -33,6 +37,7 @@ import axiosConfig from '../axios';
 export default {
   data() {
     return {
+      name: '',
       email: '',
       password: '',
       confirmPassword: ''
@@ -41,10 +46,11 @@ export default {
   methods: {
     async register() {
      try {
-      const response = await axios.post('${axiosConfig.baseUrl}/register', {
+      const response = await axios.post(`${axiosConfig.baseURL}/register`, {
+        name: this.name,
         email: this.email,
         password: this.password,
-        confirmPassword: this.confirmPassword
+        password_confirmation: this.confirmPassword
       });
 
       console.log(response.data);
