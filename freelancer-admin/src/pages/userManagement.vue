d024a0126a1785f4a8568eb5f2cc67b80519af74
mohsin khan <117224062+MohsinK443@users.noreply.github.com>
2023-08-23 22:43:19 +0500

    search bar on the admin side

--- freelancer-admin/src/pages/userManagement.vue
+++ freelancer-admin/src/pages/userManagement.vue
@@ -3,36 +3,43 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-12">
-          <div class="card shadow table-striped table-responsive">
+          <div class="card shadow">
             <div class="card-header d-flex align-items-center justify-content-between py-3 px-4 bg-light">
-              <h4 class="card-title mb-0">All Users</h4>
-              <router-link :to="{ name: 'Add User'}" class="btn btn-primary btn-sm"> + Add an Employee</router-link>
-            </div>
-            <table class="table mb-0">
-              <thead class="thead-light">
-                <tr>
-                  <th>ID</th>
-                  <th>User Name</th>
-                  <th>Email</th>
-                  <th>Role</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                  <tr v-for="user in users" :key="user.id" class="border-b border-gray-200 hover:bg-gray-100 transition-all duration-300">
-                      <td class="py-2 px-4">{{ user.id }}</td>
-                      <td class="py-2 px-4">{{ user.name }}</td>
-                      <td class="py-2 px-4">{{ user.email }}</td>
-                      <td class="py-2 px-4">{{ user.role }}</td>
-                      <td class="py-2 px-4 d-flex flex-wrap gap-2">
-                          <router-link :to="{ name: 'View User', params: { id: user.id } }" class="btn btn-info btn-sm">View</router-link>
-                          <router-link :to="{ name: 'Edit User', params: { id: user.id } }" class="btn btn-primary btn-sm">Edit</router-link>
-                          <button @click="deleteUser(user.id)" class="btn btn-danger btn-sm">Delete</button>
-                          <router-link :to="{name: 'Logged Time', params: {id: user.id }}" class="text-sm py-1 px-3 bg-blue-500 text-white rounded hover:bg-blue-600 transition-all duration-300">See Logged time</router-link>
-                      </td>
+  <h4 class="card-title mb-0">All Users</h4>
+  <div class="d-flex align-items-center">
+    <input type="text" v-model="searchQuery" placeholder="Search by name..." class="form-control mr-3" style="width: 250px;"> <!-- Adjust the width as needed -->
+    <router-link :to="{ name: 'Add User'}" class="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 transition-all duration-300">Add Employee</router-link>
+  </div>
+</div>
+
+
+            <div class="table-responsive">
+              <table class="table mb-0">
+                <thead class="thead-light">
+                  <tr>
+                    <th>ID</th>
+                    <th>User Name</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th>Actions</th>
                   </tr>
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  <tr v-for="user in filteredUsers" :key="user.id" class="border-b border-gray-200 hover:bg-gray-100 transition-all duration-300">
+                    <td class="py-2 px-4">{{ user.id }}</td>
+                    <td class="py-2 px-4">{{ user.name }}</td>
+                    <td class="py-2 px-4">{{ user.email }}</td>
+                    <td class="py-2 px-4">{{ user.role }}</td>
+                    <td class="py-2 px-4">
+                      <router-link :to="{ name: 'View User', params: { id: user.id } }" class="btn btn-outline-info btn-sm mr-2">View</router-link>
+                      <router-link :to="{ name: 'Edit User', params: { id: user.id } }" class="btn btn-outline-primary btn-sm mr-2">Edit</router-link>
+                      <button @click="deleteUser(user.id)" class="btn btn-outline-danger btn-sm mr-2">Delete</button>
+                      <router-link :to="{name: 'Logged Time', params: {id: user.id }}" class="text-sm py-1 px-3 bg-blue-500 text-white rounded hover:bg-blue-600 transition-all duration-300">See Logged time</router-link>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
@@ -42,14 +49,28 @@
 
 
 
+
+
 <script>
   import { mapActions, mapState } from 'vuex'
   import Card from 'src/components/Cards/Card.vue'
   import axios from 'axios'
   import axiosConfig from '../axios';
   export default {
+    data() {
+  return {
+    searchQuery: ''
+  };
+},
     computed: {
-      ...mapState(['users'])
+      ...mapState(['users']),
+      filteredUsers() {
+        if (this.searchQuery) {
+          const queryLower = this.searchQuery.toLowerCase();
+          return this.users.filter(user => user.name.toLowerCase().includes(queryLower));
+        }
+        return this.users;
+      }
     },
     created () {
       this.getAllUsers()
